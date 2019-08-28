9e5fdf16896e9fd0329f6597dea62b62416c69ae
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 11:16:02 -0400

    Update file

--- src/AverageInt.java
+++ src/AverageInt.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class AverageInt {
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int thirdNumber = 0;
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
 
     }
 }
