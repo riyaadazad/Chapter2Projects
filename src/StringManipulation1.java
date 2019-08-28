d87d3d72175cd4328a15d2b7172db013f5692549
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 19:33:56 -0400

    Update file

--- src/StringManipulation1.java
+++ src/StringManipulation1.java
@@ -1,5 +1,9 @@
 public class StringManipulation1 {
     public static void main(String[] args) {
-
+        String word = "Briar Woods High School";
+        System.out.println(word);
+        System.out.println("The character at position 4 is " + word.charAt(4));
+        System.out.println("The length of the string is: " + word.length());
+        System.out.println(word.concat(" is my school."));
     }
 }
