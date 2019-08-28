7660b63f9b5e87c5ed402847a9287757292326ec
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 18:45:36 -0400

    Update file

--- src/AverageCorrect.java
+++ src/AverageCorrect.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class AverageCorrect {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.print("Enter third (final) number: ");
+        int thirdNumber = input.nextInt();
+
+        int numberSum = firstNumber + secondNumber + thirdNumber;
+        int numberCount = 3;
+
+        Double avgNumber = Double.valueOf(numberSum) / Double.valueOf(numberCount);
+
+        System.out.println("The average is " + avgNumber);
+
+        /*
+        * I can't believe it worked this is so awesome!!!
+        * I used Beginners Book again to solve this problem:
+        * https://beginnersbook.com/2018/09/java-convert-int-to-double/
+        */
     }
 }
