ad9d2dc06c85e8cff5d164034a166aa506c10b31
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 11:08:21 -0400

    Update file

--- src/EscapeSequences.java
+++ src/EscapeSequences.java
@@ -1,5 +1,8 @@
 public class EscapeSequences {
-    public static void main(String[] args) {
-        System.out.println("This is simply a demonstration of \"quote marks\"." + "\n \t It also demonstrates the use of a tab character.");
+    public static void main(String[] args) { // Prints out a statement with various tabs, special characters, and new lines
+        System.out.println("\n This is simply a demonstration of \"quote marks\"." + // Added an additional \n to the beginning of the println() statement to match the example provided (i.e. when the program ran it had an extra nbsp;)
+                "\n \t It also demonstrates the use of a tab character." +
+                "\n \t Remember that the \"\\\" has a special meaning in \n java." +
+                "\n This concludes this demonstration.");
     }
 }
