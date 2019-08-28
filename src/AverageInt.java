aaa59972f4643e33c4389bbfcbbc0ff56a4c3f72
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 18:26:03 -0400

    Update file

--- src/AverageInt.java
+++ src/AverageInt.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 
 public class AverageInt {
-    int firstNumber = 0;
-    int secondNumber = 0;
-    int thirdNumber = 0;
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Uses Scanner to take in user-inputted ints and returns the average
+
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter first number: ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int secondNumber = input.nextInt();
+
+        System.out.print("Enter third (final) number: ");
+        int thirdNumber = input.nextInt();
+
+        int totalNumber = firstNumber + secondNumber + thirdNumber;
+
+        int avgNumber = totalNumber / 3; // I know this isn't practical because the amount of numbers for the user to enter can change :-)
+
+        System.out.println("The incorrect average is " + avgNumber);
+
+        /*
+        Was this supposed to be difficult because it was pretty difficult...
+        To solve this problem I looked on Stack Overflow (https://stackoverflow.com/questions/7337167/system-out-print-method-doesn%c2%b4t-work-right)
+        and Beginners Book (https://beginnersbook.com/2017/09/java-program-to-read-integer-value-from-the-standard-input/)
+        ...But once I solved it,
+         */
 
     }
 }
