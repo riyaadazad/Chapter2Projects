79f18b0c12950f86d73282811827e891da581be2
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-29 10:23:45 -0400

    Update files

--- src/EscapeSequences.java
+++ src/EscapeSequences.java
@@ -6,9 +6,9 @@ public class EscapeSequences {
     use plus signs (+) to continue your System.out.println from line to line.
     */
     public static void main(String[] args) { // Prints out a statement with various tabs, special characters, and new lines
-        System.out.println("\n This is simply a demonstration of \"quote marks\"." + // Added an additional \n to the beginning of the println() statement to match the example provided (i.e. when the program ran it had an extra nbsp;)
-                "\n \t It also demonstrates the use of a tab character." +
-                "\n \t Remember that the \"\\\" has a special meaning in \n java." +
-                "\n This concludes this demonstration.");
+        System.out.println("This is simply a demonstration of \"quote marks\"." + // Added an additional \n to the beginning of the println() statement to match the example provided (i.e. when the program ran it had an extra nbsp;)
+                "\n\tIt also demonstrates the use of a tab character." +
+                "\n\tRemember that the \"\\\" has a special meaning in \njava." +
+                "\nThis concludes this demonstration.");
     }
 }
