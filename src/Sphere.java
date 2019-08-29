77ee3ffd64fb9e2b1de1b72d550cb4138c6679e1
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 21:03:24 -0400

    Add description of assignment

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,4 +1,10 @@
 public class Sphere {
+    /*
+    Assignment: Write an application that reads the radius of a sphere (in inches) and prints its volume and surface
+    area. Use the DecimalFormat class to print the output to four decimal places. r represents the radius.
+    Volume = (4/3)(pi)(r)^3
+    Surface Area = 4(pi)(r)^2
+    */
     public static void main(String[] args) {
 
     }
