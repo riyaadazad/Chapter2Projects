3f64254388c902464df5cba2b80c56a00ed08bb2
Riyaad Azad <azad.riyaad@gmail.com>
2019-08-28 21:03:08 -0400

    Add description of assignment

--- src/StringManipulation1.java
+++ src/StringManipulation1.java
@@ -1,32 +1,74 @@
-public class StringManipulation1 { // BTW The assignment says to print out all 14 output prompts but there are only 13
-    public static void main(String[] args) {
+public class StringManipulation1 {
+    /*
+    Assignment: Write a program to do the following: ** Print out all 14 output prompts.
+    */
+    public static void main(String[] args) { // BTW The assignment says to print out all 14 output prompts but there are only 13
+        // a. Create a String called word and give it a value. The value should be "Briar Woods High School".
         String word = "Briar Woods High School";
         System.out.println(word);
 
+        // b. Print out the character at position 4 in the String. Use the .charAt() method.
         System.out.println("The character at position 4 is " + word.charAt(4));
 
+        // c. Print out the length of the string. Use the .length() method.
         System.out.println("The length of the string is: " + word.length());
 
+        /*
+        d. Concatenate " is my school." to the end of your String and store the new String into word.
+        The resulting String should be "Briar Woods High School is my school."
+        Use the .concat() method to do this. Print out the resulting String.
+        */
         word = word.concat(" is my school.");
         System.out.println(word);
 
+        /*
+        e. Concatenate "Because I live in Loudoun County," to the beginning of your String and store the new String into
+        word. The resulting String should read "Because I live in Loudoun County, Briar Woods High School is my school."
+        Use the .concat() method to do this. Print out the resulting String.
+        */
         word = "Because I live in Loudoun County, ".concat(word); // https://beginnersbook.com/2013/12/java-string-concat-method-example/
         System.out.println(word);
 
+        // f. Print out the length of the String. Use the .length() method.
         System.out.println("The length of the string is: " + word.length());
 
+        /*
+        g. Print out the substring of your string starting with the character at position 23.
+        (Remember that Strings start at position 0, so the first character in your string will be a "u".)
+        */
         System.out.println("word.substring(23) is: ");
 
+        /*
+        h. Print out the substring of your string starting with the character in position 23 and
+        ending with the character in position 33. Be careful to use the substring method correctly.
+        This should print out "un County".
+        */
         System.out.println("word.substring(23, 33) is: ");
 
+        // i. Print out the location of the word "school" in your String. Use the .indexOf() Method to do this.
         System.out.println("The location of the word \"school\" is at ");
 
+        /*
+        j. Print out the location of the word "School" in your String. Use the .indexOf() Method to do this.
+        Notice this answer is not the same as the last one. Do you know why not?
+        */
         System.out.println("The location of the word \"School\" is at ");
 
+        /*
+        k. Convert every occurrence of the letter "o" to "x" in your String. Use the .replace() method to do this.
+        Print out the resulting String.
+        */
         System.out.println("\"o\" is replaced with \"x\": ");
 
+        // Convert your String to upper case. Print out the resulting String.
         System.out.println("Final Output: ");
 
+        // Print out the length of the final String using the .length() method.
         System.out.println("Final Length: ");
+
+        /*
+        Did you know... You can chain some methods together. For example:
+        word.substring(3, 20).equals(otherWord.substring(6) + "hello");
+        */
     }
 }
--- src/StringManipulation2.java
+++ src/StringManipulation2.java
@@ -1,5 +1,23 @@
-public class StringManipulation2 {
+public class StringManipulation2 { // Assignment: Create three string objects in your program. Their values should be: (not shown.)
     public static void main(String[] args) {
-
+        // a. Find the character at location 27 in string2. Print it out.
+        /*
+        b. Append string3 to the end of string2 to the end of string1. Store these
+        in a 4th string called string4. Print out the resulting string.
+        */
+        // c. Compare string2 to string4 to see if they are EQUAL using .equals(). Print out the result.
+        /*
+        d. Compare string2 to string3 to see which comes first using compareTo(). Print out the result
+        (a negative number, 0 or a positive number).
+        */
+        // e. Create string5. Its value should be "this is only a test." Print out string5.
+        // f. Compare string3 to string5 to see if they are EQUAL using .equals(). Print out the result.
+        // g. Compare string3 to string5 to see if they are equal if you ignore cases. Print out the result.
+        // h. See if the string "he" exists in string1. Print out the numerical result.
+        // i. See if the string "he" exists in string2. Print out the numerical result.
+        // j. Replace all occurrences of "he" with "xyzzy" in string 2. Print out the result.
+        // k. Determine the length of string 1 and print out the result.
+        // l. Print out part of string1, starting at the 3rd character all the way to the end.
+        // m. Print out part of string2, starting at position 2 and going through position 10.
     }
 }
